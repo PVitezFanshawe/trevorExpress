79b2a3db0d5faea5782ac496ee047888c302b1b9
Vitez <p_vitez@tss-hr420-dm28.fc.ca>
2018-02-12 12:33:14 -0500

    semibroken ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  {name: "Jade", usefulness : "pretty ok"},
+  {name: "EJS", usefulness : "too many <%"},
+  {name: "Handlebars", usefulness : "i can ride my bike with no"}
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: 'baby dont hurt me' });
+  res.render('index', { 
+    title: 'Express',
+    message: 'not broken',
+    template: templateTypes
+  });
 });
 
 module.exports = router;
